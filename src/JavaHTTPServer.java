14e6c5dcad69f1516d87646fc19d9dbd36358bc3
maw101 <maw101@aber.ac.uk>
2019-08-10 14:10:08 +0100

    Create basic server to listen for connection

--- src/JavaHTTPServer.java
+++ src/JavaHTTPServer.java
@@ -1,7 +1,14 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class JavaHTTPServer {
 
     public static void main(String[] args) throws IOException {
-
+        final ServerSocket server = new ServerSocket(8080);
+        System.out.println("Listening for connection on port 8080.");
+        while(true) {
+            // keep connection open until server stopped
+        }
     }
 
 }
