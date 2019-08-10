2dad646a05900efca2e445893ad1e85994e98d95
maw101 <maw101@aber.ac.uk>
2019-08-10 14:26:09 +0100

    Read the GET request

--- src/JavaHTTPServer.java
+++ src/JavaHTTPServer.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -11,6 +13,15 @@ public class JavaHTTPServer {
             // accept incoming connections
             final Socket client = server.accept();
             // read request, prepare response, send response, close socket
+
+            // read GET request
+            InputStreamReader is_reader = new InputStreamReader(client.getInputStream());
+            BufferedReader reader = new BufferedReader(is_reader); // as request is multiple lines, use a BR
+            String requestLine = reader.readLine();
+            while (!requestLine.isEmpty()) {
+                System.out.println(requestLine);
+                requestLine = reader.readLine();
+            }
         }
     }
 
