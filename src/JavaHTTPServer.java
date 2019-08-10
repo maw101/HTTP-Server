da83973328a298a8549444362492f0c3e0aa3855
maw101 <maw101@aber.ac.uk>
2019-08-10 14:30:43 +0100

    Prepare response

--- src/JavaHTTPServer.java
+++ src/JavaHTTPServer.java
@@ -10,17 +10,26 @@ public class JavaHTTPServer {
         final ServerSocket server = new ServerSocket(8080);
         System.out.println("Listening for connection on port 8080.");
         while(true) {
-            // accept incoming connections
-            final Socket client = server.accept();
             // read request, prepare response, send response, close socket
 
-            // read GET request
+            /* read GET request
+            // accept incoming connections
+            final Socket client = server.accept();
             InputStreamReader is_reader = new InputStreamReader(client.getInputStream());
             BufferedReader reader = new BufferedReader(is_reader); // as request is multiple lines, use a BR
             String requestLine = reader.readLine();
             while (!requestLine.isEmpty()) {
                 System.out.println(requestLine);
                 requestLine = reader.readLine();
+            }
+            */
+            
+            try (Socket client = server.accept()) {
+                // prepare response
+                String httpResponse = "HTTP/1.1 200 OK\n";
+                long millis = System.currentTimeMillis();
+
+
             }
         }
     }
